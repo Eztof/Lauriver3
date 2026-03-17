987f8ab55b1a84b91e91178d7a1ba1645efe593b
Eztof <90273638+Eztof@users.noreply.github.com>
2026-03-17 16:56:34 +0000

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.oliver.lauriver3"
         minSdk = 31
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
